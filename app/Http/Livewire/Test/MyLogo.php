25b5a254b40912b4a7a527b3b243d087a02dd3bb
kulyaginv <kulyaginv@gmail.com>
2023-04-07 09:49:13 +0300

    first commit

---
+++ app/Http/Livewire/Test/MyLogo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Test;
+
+use Livewire\Component;
+
+class MyLogo extends Component
+{
+    public function render()
+    {
+        return view('livewire.test.my-logo');
+    }
+}
---
+++ resources/views/livewire/test/my-logo.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <img src="{{ Storage::url('img/logo.jpg')}}" width="48px" height="48px" style="border-radius: 50%;" alt="logo">
+</div>
