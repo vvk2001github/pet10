0478223e6a4cb33824a72162a27c8049e9abdb75
kulyaginv <kulyaginv@gmail.com>
2023-04-19 15:37:07 +0300

    Chat - The First Steps

---
+++ app/Models/Message.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'sender_id',
+        'conversation_id',
+        'body',
+        'last_time_message',
+    ];
+
+    public function conversation():BelongsTo
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function sender():BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+}
---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Message;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+
+    protected $model = Message::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'sender_id' => $this->faker->numberBetween(1, 2),
+            'conversation_id' => $this->faker->randomElement([1, 3]),
+            'body' => $this->faker->text(1000),
+        ];
+    }
+}
