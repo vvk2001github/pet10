7b8f58fc9aff8c6d263a813164c4f047d1dcca09
kulyaginv <kulyaginv@gmail.com>
2023-05-29 14:09:07 +0300

    Favicon

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,16 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{__('Test')}}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="bg-gray-200 bg-opacity-25 flex items-center justify-center gap-6 lg:gap-8 p-6 lg:p-8" >
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                <div class="flex items-center justify-center gap-6 p-6 bg-gray-200 bg-opacity-25 lg:gap-8 lg:p-8" >
 
-                    <img src="{{ Storage::url('img/roboface.jpg') }}" class="rounded-lg w-3/12 hover:w-3/4">
+                    <img src="{{ Storage::url('img/roboface.jpg') }}" class="w-3/12 rounded-lg hover:w-3/4">
 
                 </div>
             </div>
